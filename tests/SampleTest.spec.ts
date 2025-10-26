9afa2304b14af3520591c02e135b61b031bc0073
SOORYA ASOKAN <75926722+soorya474@users.noreply.github.com>
2025-10-26 10:32:39 +0000

    Updates

--- tests/SampleTest.spec.ts
+++ tests/SampleTest.spec.ts
@@ -1,5 +1,5 @@
 import test from "@playwright/test";
-import {HomePage }from "../pages/HomePage.spec"
+import { HomePage } from "../pages/HomePage.spec"
 import { HolidaysPage } from "../pages/HolidaysPage.spec";
 import { HotelPage } from "../pages/HotelPage.spec";
 import { CustomiseHolidaysPage } from "../pages/CustomiseHolidayPage.spec";
@@ -7,28 +7,28 @@ import { PassengerDetails } from "../pages/PassengerDetailsPage.spec";
 import { TestData } from "../testData/testData.spec";
 
 
-test('Login test',async({page})=>{
-const home=new HomePage(page);
-const holPage=new HolidaysPage(page);
-const hotelpg=new HotelPage(page);
-const custHols=new CustomiseHolidaysPage(page);
-const PassdEtails=new PassengerDetails(page);
-await home.navigateToHomePage();
-await home.clickOnAcceptBtn();
-await home.selectDepartureAirport();
-await home. clickOnAllowDeals();
+test('Login test', async ({ page }) => {
+    const home = new HomePage(page);
+    const holPage = new HolidaysPage(page);
+    const hotelpg = new HotelPage(page);
+    const custHols = new CustomiseHolidaysPage(page);
+    const passdetails = new PassengerDetails(page);
+    await home.navigateToHomePage();
+    await home.clickOnAcceptBtn();
+    await home.selectDepartureAirport();
+    await home.clickOnAllowDeals();
 
-await home.selectDestination();
-await home.selDepartureDate();
-await home.selNoOfPersons();
-await home.selectChildAge(TestData.tc01.childAge);
-await home.clickOnSearchBtn();
-//await holPage.clickOnContinue();
+    await home.selectDestination();
+    await home.selDepartureDate();
+    await home.selNoOfPersons();
+    await home.selectChildAge(TestData.tc01.childAge);
+    await home.clickOnSearchBtn();
+    await holPage.clickOnContinue();
 
-await hotelpg.clickOnContinueBtn();
-await custHols.clickOnContinueDetails();
-await PassdEtails.clickOnContinuePay();
-await PassdEtails.valErrorMsgIsPresent();
+    await hotelpg.clickOnContinueBtn();
+    await custHols.clickOnContinueDetails();
+    await passdetails.clickOnContinuePay();
+    await passdetails.valErrorMsgIsPresent();
 
 
 
